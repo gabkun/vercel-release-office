d2677d68680480e12ef3bd53c1945fcd5220f0eb
gabkun <95224438+gabkun@users.noreply.github.com>
2025-06-16 19:49:19 +0800

    idk men

--- src/Admin/Modals/DateFilter.jsx
+++ src/Admin/Modals/DateFilter.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-
+import { DateRange } from "react-date-range";
+import { enUS } from "date-fns/locale";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 export const DateRangeModal = ({ isOpen, onClose, dateRange, setDateRange }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-ss
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-full w-[90%] md:w-[30rem]">
+        <h2 className="text-lg font-semibold mb-4">Select Date Range</h2>
+
+        <DateRange
+          editableDateInputs={true}
+          onChange={(item) => setDateRange([item.selection])}
+          moveRangeOnFirstSelection={false}
+          ranges={dateRange}
+          locale={enUS}
+        />
+
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={onClose}
+            className="bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded"
+          >
+            Close
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
